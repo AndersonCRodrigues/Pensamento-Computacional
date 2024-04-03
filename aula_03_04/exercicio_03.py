e9f7e749f37f1c786825d83ced0805b0416c9c06
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2024-04-03 15:56:07 -0300

    feat: exercicio 03

---
+++ aula_03_04/exercicio_03.py
@@ -0,0 +1,11 @@
+"""
+Em épocas de pouco dinheiro, os comerciantes estão procurando aumentar suas
+vendas oferecendo desconto. Faça um algoritmoque possa receber um valor de um
+produto e que escreva o novo valor tendo em vista que o desconto foi de 9%.
+"""
+
+valor = float(input("Entre com o valor a ser calculado: "))
+
+desconto = valor * 0.91
+
+print(f"{valor} com desconto de 9% é igual à {desconto}")
