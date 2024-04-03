3d422ef1298295d94903dbe1285440d187131f24
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2024-04-03 16:14:47 -0300

    feat: exercicio 05

---
+++ aula_03_04/exercicio_05.py
@@ -0,0 +1,24 @@
+"""
+Uma pessoa resolveu fazer uma aplicação em uma poupança programada.
+Para calcular seu rendimento, ela deverá fornecer o valor constante da
+aplicação mensal, a taxa e o número de meses. Sabendo-se que a fórmula
+usada para este cálculo é:
+"""
+
+
+def calculo_poupanca(p, i, n):
+    calc = (p * (1 + i) ** n - 1) / 7
+    return calc
+
+
+valor = float(input("Entre com o valor a ser aplicar mensalmente: R$ "))
+taxa = float(input("Entre com a taxa a ser aplicada: "))
+meses = int(input("Entre com o número de meses: "))
+
+aplicacao = calculo_poupanca(valor, taxa, meses)
+
+print(
+    f"O valor de R$ {valor}, "
+    f"aplicado a uma taxa de {taxa}% ao longo de {meses} meses, "
+    f"renderá R$ {aplicacao:.2f}"
+)
