81dc43e9e76162d82523180347b1831a48aa1776
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2024-04-03 17:02:57 -0300

    feat: exercicio 15

---
+++ aula_03_04/exercicio_15.py
@@ -0,0 +1,10 @@
+"""
+Faça um algoritmo que apresente, para um salário informado pelo usuário, um
+novo salário com aumento de 37%.
+"""
+
+salario = float(input("Entre com o salário atua: R$ "))
+
+ajuste = salario * 1.37
+
+print(f"Salário foi reajustado para R$ {ajuste:.2f}")
