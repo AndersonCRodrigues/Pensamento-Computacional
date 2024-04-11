4d7dd57bdacb85a04174deb19a1d9267fcec0c55
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2024-04-11 08:37:50 -0300

    fix: calculo da poupança

--- aula_03_04/exercicio_05.py
+++ aula_03_04/exercicio_05.py
@@ -7,18 +7,17 @@ usada para este cálculo é:
 
 
 def calculo_poupanca(p, i, n):
-    calc = (p * (1 + i/100) ** n - 1
-    return calc
+    return (p * ((1 + i / 100) ** n-1)) / i
 
 
-valor = float(input("Entre com o valor a ser aplicar mensalmente: R$ "))
+valor = float(input("Entre com o valor a ser aplicado mensalmente: R$ "))
 taxa = float(input("Entre com a taxa a ser aplicada: "))
 meses = int(input("Entre com o número de meses: "))
 
 aplicacao = calculo_poupanca(valor, taxa, meses)
 
 print(
-    f"O valor de R$ {valor}, "
+    f"O valor de R$ {valor:.2f}, "
     f"aplicado a uma taxa de {taxa}% ao longo de {meses} meses, "
     f"renderá R$ {aplicacao:.2f}"
 )
